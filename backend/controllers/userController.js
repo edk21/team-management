4212dac969e3c37a2d41fce49ba0503ee511b0ee
edk21 <edwinmcken@gmail.com>
2023-06-05 10:07:20 +0200

    logout fontions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,7 +118,23 @@ const loginUser = asyncHandler( async (req, res) => {
     }
 })
 
+
+const logoutUser = asyncHandler( async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: "none",
+        secure: true
+    })
+
+    return res.status(200).json({
+        message: "You have successfully logged out"
+    })
+})
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
